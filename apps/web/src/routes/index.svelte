be7ef9657c63e8841f1231c9c53c0e0d9cc4f729
Kayaba Attribution <jdavidgomezca@gmail.com>
2022-02-10 11:22:41 -0500

    feat: add tailwindcss on svelte main

--- apps/web/src/routes/index.svelte
+++ apps/web/src/routes/index.svelte
@@ -1,10 +1,63 @@
-<script lang="ts">
-    // your script goes here
+<script context="module">
+	export const prerender = true;
 </script>
 
-<h1>Welcome to SvelteKit Hey why is this happening</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+
+		<h1 class="text-3xl font-bold underline">
+			Hello this is wow cool rld!
+		</h1>
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
 
 <style>
-    /* your styles go here */
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
 </style>
